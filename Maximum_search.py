2f4ecbbed9d1031bf01fbf4e5482978a7051ccab
abigaelle-lagneaux-GEODES <abigaelle.lagneaux@usherbrooke.ca>
2025-05-05 11:18:41 -0400

    MaJ v3

---
+++ Maximum_search.py
@@ -0,0 +1,133 @@
+######################################################################################################################################################################################
+#################################################################################### IMPORTATIONS ####################################################################################
+######################################################################################################################################################################################
+
+import skimage as sk
+
+import rasterio
+
+import numpy as np
+
+from shapely.geometry import Point, LineString, Polygon
+
+######################################################################################################################################################################################
+######################################################################################## CODE ########################################################################################
+######################################################################################################################################################################################
+
+def horizontal_90(min_pos, masked_image, no_data_value, out_transform):
+    # Pour la ligne de pixels extraite, trouver les valeurs des colonnes à droite de la position minimale dans la même ligne
+    # Index de la ligne où se trouve l'altitude la plus basse
+    row_index = min_pos[1]
+    # Extraction de la ligne à partir de la position min_pos vers la droite
+    line = masked_image[0, row_index, min_pos[2]:]
+
+    # Filtrer les valeurs "no data" de la ligne
+    line_values_rg = line[line != no_data_value]
+
+    if line_values_rg.size > 0:
+        # Trouver l'altitude la plus haute dans la ligne filtrée
+        max_val_right = line_values_rg.max()  # Trouver l'altitude la plus haute dans cette ligne
+        max_pos_right = (row_index, min_pos[2] + np.where(line == max_val_right)[0][
+            0])  # Calculer la position de l'altitude la plus haute
+
+        # Vérifier si c'est la dernière valeur de la ligne
+        derniere_val_line = np.where(line == max_val_right)[0][0]
+        range_dern_val_line = derniere_val_line in {len(line) - 1, len(line) - 2, len(line) - 3}
+
+        # Convertir les indices en coordonnées réelles
+        max_x_rg, max_y_rg = rasterio.transform.xy(out_transform, max_pos_right[0], max_pos_right[1])
+
+        # Créer un objet Point
+        point_haut_horiz_90 = Point(max_x_rg, max_y_rg)
+
+        # Ajouter le point avec l'altitude la plus basse à la liste
+        # highest_points.append({'geometry': point_haut_horiz_90, 'max_altitude_right': max_val_right, 'run_id': id, 'NAC_DTM_ID': nac_id})
+
+        return max_val_right, point_haut_horiz_90
+
+def horizontal_270(min_pos, masked_image, no_data_value, out_transform):
+    row_index = min_pos[1]
+    # Trouver les valeurs des colonnes à droite de la position minimale dans la même ligne
+    line_lf = masked_image[0, row_index,
+              :min_pos[2] + 1]  # Extraire la ligne à partir de la position min_pos vers la gauche
+
+    # Filtrer les valeurs "no data" de la ligne
+    line_values_lf = line_lf[line_lf != no_data_value]
+
+    if line_values_lf.size > 0:
+        # Trouver l'altitude la plus haute dans la ligne filtrée
+        max_val_left = line_values_lf.max()  # Trouver l'altitude la plus haute dans cette ligne
+        max_pos_left = (
+        row_index, np.where(line_lf == max_val_left)[0][0])  # Calculer la position de l'altitude la plus haute
+
+        # Vérifier si c'est la première valeur de la ligne
+        premiere_val_line = np.where(line_lf == max_val_left)[0][0]
+        range_prem_val_line = premiere_val_line in {0, 1, 2}
+
+        # Convertir les indices en coordonnées réelles
+        max_x_lf, max_y_lf = rasterio.transform.xy(out_transform, max_pos_left[0], max_pos_left[1])
+
+        # Créer un objet Point
+        point_haut_horiz_270 = Point(max_x_lf, max_y_lf)
+
+        return max_val_left, point_haut_horiz_270
+
+def vertical_360 (min_pos, masked_image, no_data_value, out_transform):
+    # Trouver les valeurs des colonnes en haut de la position minimale dans la même colonne
+    col_top_index = min_pos[2]  # Index de la colonne où se trouve l'altitude la plus basse
+    col_top = masked_image[0, :min_pos[1] + 1,
+              col_top_index]  # Extraire la colonne à partir de la position min_pos vers le haut
+
+    # Filtrer les valeurs "no data" de la ligne
+    col_values_top = col_top[col_top != no_data_value]
+
+    if col_values_top.size > 0:
+        # Trouver l'altitude la plus haute dans la ligne filtrée
+        max_val_top = col_values_top.max()  # Trouver l'altitude la plus haute dans cette ligne
+        max_pos_top = (
+        np.where(col_top == max_val_top)[0][0], col_top_index)  # Calculer la position de l'altitude la plus haute
+
+        # Vérifier si c'est la première valeur de la colonne
+        premiere_val_col = np.where(col_top == max_val_top)[0][0]
+        range_prem_val_col = premiere_val_col in {0, 1, 2}
+
+        # Convertir les indices en coordonnées réelles
+        max_x_top, max_y_top = rasterio.transform.xy(out_transform, max_pos_top[0], max_pos_top[1])
+
+        # Créer un objet Point
+        point_haut_vert_360 = Point(max_x_top, max_y_top)
+
+        # Ajouter le point avec l'altitude la plus basse à la liste
+        # highest_points.append({'geometry': point_haut_vert_360, 'max_altitude_right': max_val_top, 'run_id': id, 'NAC_DTM_ID': nac_id})
+
+        return max_val_top, point_haut_vert_360
+
+def vertical_180 (min_pos, masked_image, no_data_value, out_transform):
+    # Trouver les valeurs des lignes en dessous de la position minimale dans la même colonne
+    col_bas_index = min_pos[2]  # Index de la colonne où se trouve l'altitude la plus basse
+    col_bas = masked_image[0, min_pos[1]:,
+              col_bas_index]  # Extraire la colonne à partir de la position min_pos vers le bas
+
+    # Filtrer les valeurs "no data" de la colonne
+    col_values_bas = col_bas[col_bas != no_data_value]
+
+    if col_values_bas.size > 0:
+        # Trouver l'altitude la plus haute dans la colonne filtrée
+        max_val_bas = col_values_bas.max()  # Trouver l'altitude la plus haute dans cette colonne
+        max_pos_bas = (min_pos[1] + np.where(col_bas == max_val_bas)[0][0],
+                       col_bas_index)  # Calculer la position de l'altitude la plus haute
+
+        # Vérifier si c'est la dernière valeur de la colonne
+        derniere_val_col = np.where(col_bas == max_val_bas)[0][0]
+        range_derniere_val_col = derniere_val_col in {len(col_bas) - 1, len(col_bas) - 2, len(col_bas) - 3}
+
+        # Convertir les indices en coordonnées réelles
+        max_x_bas, max_y_bas = rasterio.transform.xy(out_transform, max_pos_bas[0], max_pos_bas[1])
+
+        # Créer un objet Point
+        point_haut_vert_180 = Point(max_x_bas, max_y_bas)
+
+        return max_val_bas, point_haut_vert_180
+
+
+
